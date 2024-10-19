c17b6d557afbf44cdccbba10af5a412a8ad0f87e
Melissa Cote <diamondmeadows22@gmail.com>
2024-10-19 00:15:10 -0400

    main file with encode function and main logic

---
+++ main.py
@@ -0,0 +1,48 @@
+
+# Prints the menu and prompts user for menu selection.
+def menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print()
+    choice = int(input("Please enter an option: "))
+    return choice
+
+# Takes in a password in string format containing only integers. Encodes by converting each digit in the password to an
+# integer, adding 3, then converting the digit back to a string. Each digit is added to the variable "encoded", which
+# stores the final encoded password.
+
+def encode():
+    original = input("Please enter your password to encode: ")
+    encoded = ""
+    for digit in original:
+        encoded += str(int(digit) + 3)
+    print("Your password has been encoded and stored!")
+    return encoded
+
+def decode(encoded):
+    pass
+
+def main():
+    menu_selection = menu()
+    while True:
+        if menu_selection == 1:
+            encoded_password = encode()
+            print()
+            menu_selection = menu()
+
+        elif menu_selection == 2:
+            decode(encoded_password)
+            print()
+            menu_selection = menu()
+
+        elif menu_selection == 3:
+            break
+
+
+
+
+if __name__ == '__main__':
+    main()
